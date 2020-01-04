55253dd50598a6e670f5390b76b74850b73537c6
ybuchi <yabuch_ch@hotmail.com>
2020-01-04 12:59:22 -0500

    Added a few extra changes to components & the landing page

--- client/src/components/LandingInfoPanel/index.js
+++ client/src/components/LandingInfoPanel/index.js
@@ -6,10 +6,13 @@ function Jumbotron({ children }) {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-md-6" id="panel-jumbo">
-
+        <div className="col-md-12">
+        {/* <div
+          className="jumbotron" id = "panel-jumbo"
+        > */}
           {children}
           
+        {/* </div> */}
       </div>
       </div>
     </div>
