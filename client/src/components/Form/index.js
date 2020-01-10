ed6866c1a21d4d55214a908838a61aef2c10ef63
ybuchi <yabuch_ch@hotmail.com>
2020-01-09 20:20:22 -0500

    Reduced the width of the input columns for login. Added a header and replaced the placeholders (alsin in login)

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -4,8 +4,8 @@ import React from "react";
 
  function Input(props) {
   return (
-    <div className="form-group">
-      <input className="form-control" {...props} />
+    <div className="form-group ">
+      <input className="form-control" {...props}/>
     </div>
   );
 }
