5ca2986612bee0868982242fd433d2faa2f250b5
Linag94 <52180242+Linag94@users.noreply.github.com>
2020-01-09 20:22:06 -0500

    [-__-}]

--- client/src/components/CookBookCard/index.js
+++ client/src/components/CookBookCard/index.js
@@ -9,9 +9,7 @@ function CookBookCard(props) {
       <div class="card-body">
         <h5 class="card-title">Recipes</h5>
         <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="card-link">Button Here</a>
-        <a href="#" class="card-link">Button There</a>
+        
 
         { searchUserRecipes(authenticated._id)}
         {/* <button onClick={() => searchUserRecipes(authenticated._id)}>Display</button> */}
@@ -28,7 +26,7 @@ function CookBookCard(props) {
             </ol>
           </>)
             : (
-              <p>Need to login first</p>
+              <p></p>
             )}
         </div>
       </div>
